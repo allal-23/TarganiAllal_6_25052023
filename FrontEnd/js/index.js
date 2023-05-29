df40391f7a7555fd324fac6ee598641aae7f96f6
allal-23 <75438371+allal-23@users.noreply.github.com>
2023-05-29 23:13:20 +0200

    affichage des données récupérées
    affichage des données récupérées dans le HTML et dans un console.log

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,7 +1,46 @@
+// fetch("http://localhost:5678/api/works", { method: "GET" })
+//   .then((data) => {
+//     return data.json();
+//   })
+//   .then((projets) => {
+//     console.log(projets);
+//   });
+//  let HTML= document.getElementById("cartes")
+
+// let myHTML=""
+
+// projets.forEach(projet => {
+//     console.log(projet.imageUrl)
+//     console.log(projet.title)
+//     myHTML +=`<figure>
+//     <img src="${projet.imageUrl}" alt="${projet.title}" />
+//     <figcaption>${projet.title}</figcaption>
+//   </figure>`
+    
+// });
+
+// console.log(myHTML)
+// HTML.innerHTML=myHTML
+
 fetch("http://localhost:5678/api/works", { method: "GET" })
   .then((data) => {
     return data.json();
   })
   .then((projets) => {
     console.log(projets);
+
+    let HTML = document.getElementById("cartes");
+    let myHTML = "";
+
+    projets.forEach((projet) => {
+      console.log(projet.imageUrl);
+      console.log(projet.title);
+      myHTML += `<figure>
+        <img src="${projet.imageUrl}" alt="${projet.title}" />
+        <figcaption>${projet.title}</figcaption>
+      </figure>`;
+    });
+
+    console.log(myHTML);
+    HTML.innerHTML = myHTML;
   });
