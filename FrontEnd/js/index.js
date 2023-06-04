626af9e70b91da6d9fcd90b68294aab6fc0aee8a
allal-23 <75438371+allal-23@users.noreply.github.com>
2023-06-04 16:22:23 +0200

    Ajout des categorie

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -32,18 +32,33 @@ fetch("http://localhost:5678/api/works", { method: "GET" })
     let HTML = document.getElementById("cartes");
     let myHTML = "";
 
+    // const monSet = new Set();
+    const test =[];
+
+   
+
     projets.forEach((projet) => {
-      console.log(projet.imageUrl);
-      console.log(projet.title);
+    //   console.log(projet.imageUrl);
+    //   console.log(projet.title);
       myHTML += `<figure>
         <img src="${projet.imageUrl}" alt="${projet.title}" />
         <figcaption>${projet.title}</figcaption>
       </figure>`;
+
+    //   monSet.add(projet.category); 
+      test.push(projet.category); 
     });
 
-    console.log(myHTML);
+    console.log(test);
     HTML.innerHTML = myHTML;
+
+
+    // Créer un nouveau tableau a partir des projet
+
+
   });
+
+
   fetch("http://localhost:5678/api/categories", { method: "GET" })
   .then((response) => {
     return response.json();
@@ -51,3 +66,5 @@ fetch("http://localhost:5678/api/works", { method: "GET" })
   .then((categories) => {
     console.log(categories);
   });
+
+
