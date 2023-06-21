e745db07e054b8780e3cd9089a7aa198cf275445
allal-23 <75438371+allal-23@users.noreply.github.com>
2023-06-21 18:39:15 +0200

    Modale d'ajout

--- FrontEnd/js/modale.js
+++ FrontEnd/js/modale.js
@@ -1,61 +1,36 @@
-// JavaScript
-document.getElementById("modifierBtn").addEventListener("click", function() {
-    document.getElementById("modal").style.display = "block";
-  });
-  
-  document.getElementsByClassName("close")[0].addEventListener("click", function() {
-    document.getElementById("modal").style.display = "none";
-  });
-  
-  window.addEventListener("click", function(event) {
-    if (event.target == document.getElementById("modal")) {
-      document.getElementById("modal").style.display = "none";
-    }
-  });
-  
-  document.getElementById("ajouterForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-  
-    // Récupérer les valeurs du formulaire
-    var title = document.getElementById("title").value;
-    var imageUrl = document.getElementById("imageUrl").value;
-    var categoryId = document.getElementById("categoryId").value;
-  
-    // Vérifier si toutes les informations nécessaires sont présentes
-    if (title.trim() === '' || imageUrl.trim() === '' || categoryId.trim() === '') {
-      // Afficher un message d'erreur si le formulaire n'est pas correctement rempli
-      alert("Veuillez remplir tous les champs du formulaire.");
-      return;
-    }
-  
-    // Créer l'objet de données à envoyer à l'API
-    var newData = {
-      title: title,
-      imageUrl: imageUrl,
-      categoryId: categoryId,
-      userId: 0 // Remplacez par l'ID de l'utilisateur approprié
-    };
-  
-    // Envoyer les données à l'API (exemple avec fetch)
-    fetch("http://localhost:5678/api/works", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(newData)
-    })
-    .then(response => response.json())
-    .then(data => {
-      // Traiter la réponse de l'API et mettre à jour le DOM
-      console.log(data);
-      // Afficher la nouvelle image dans la galerie
-      // ...
-      // Fermer la fenêtre modale
-      document.getElementById("modal").style.display = "none";
-    })
-    .catch(error => {
-      console.log(error);
-      // Afficher un message d'erreur si la requête échoue
-    });
-  });
-  
+document.querySelector(".ouvrir").addEventListener("click", function() {
+  document.querySelector(".modal").style.display = "flex";
+});
+
+document.querySelector(".modal__close").addEventListener("click", function() {
+  document.querySelector(".modal").style.display = "none";
+});
+document.querySelector(".ouvrir").addEventListener("click", function() {
+  const modalBody = document.querySelector(".modal__body");
+  modalBody.innerHTML = ""; // Réinitialise le contenu de la modal__body
+
+  const cartes = document.getElementById("cartes").innerHTML;
+  modalBody.innerHTML = cartes;
+
+  document.querySelector(".modal").style.display = "flex";
+});
+
+document.querySelector(".modal__close").addEventListener("click", function() {
+  document.querySelector(".modal").style.display = "none";
+});
+
+document.querySelector(".modal__footer_supprimer").addEventListener("click", function() {
+  const modalBody = document.querySelector(".modal__body");
+  modalBody.innerHTML = "";
+});
+document.querySelector(".modal__footer_button").addEventListener("click", function() {
+  // Afficher la deuxième modale
+  document.querySelector(".modal2").style.display = "flex";
+
+  // Disparition de la première modale
+  document.querySelector(".modal").style.display = "none";
+});
+document.querySelector(".modal2__close").addEventListener("click", function() {
+  document.querySelector(".modal2").style.display = "none";
+});
+    
