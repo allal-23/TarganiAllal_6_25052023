58591a0751810926c067203aa15dfff24fc44bc3
allal-23 <75438371+allal-23@users.noreply.github.com>
2023-06-28 21:32:02 +0200

    modale optimisation

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,5 +1,5 @@
 let projets;
-let categoriesArray = ["Tous"]; // Ajouter la catégorie "Tous" initialement
+let categoriesArray = ["Tous"];
 
 fetch("http://localhost:5678/api/works", { method: "GET" })
   .then((response) => response.json())
@@ -10,9 +10,7 @@ fetch("http://localhost:5678/api/works", { method: "GET" })
     let cartesHTML = document.getElementById("cartes");
 
     projets.forEach((projet) => {
-      
-
-      createFigure(projet, cartesHTML)
+      createFigure(projet, cartesHTML);
 
       if (!categoriesArray.includes(projet.category.name)) {
         categoriesArray.push(projet.category.name);
@@ -28,6 +26,9 @@ fetch("http://localhost:5678/api/works", { method: "GET" })
 
       link.textContent = categorie;
       link.href = "#" + categorie.toLowerCase();
+      if (categorie === "Tous") {
+        link.classList.add("active");
+      }
 
       span.appendChild(link);
       div.appendChild(span);
@@ -48,19 +49,61 @@ fetch("http://localhost:5678/api/works", { method: "GET" })
         }
 
         updateDisplayedProjects(filteredProjects);
+
+        // Supprimer la classe "active" de tous les liens
+        const links = document.querySelectorAll("#options a");
+        links.forEach((link) => {
+          link.classList.remove("active");
+        });
+
+        // Ajouter la classe "active" au lien sélectionné
+        link.classList.add("active");
       });
     });
 
     console.log("Nombre de projets:", projets.length);
     console.log("Nombre de catégories:", categoriesArray.length);
   });
+// ...
+
+fetch("http://localhost:5678/api/works", { method: "GET" })
+  .then((response) => response.json())
+  .then((data) => {
+    projets = data;
+    console.log(projets);
+
+    let cartesHTML = document.getElementById("cartes");
+
+    projets.forEach((projet) => {
+      createFigure(projet, cartesHTML);
+
+      if (!categoriesArray.includes(projet.category.name)) {
+        categoriesArray.push(projet.category.name);
+      }
+    });
+
+    let selectCategory = document.getElementById("categoryId");
+
+    categoriesArray.forEach((categorie) => {
+      let option = document.createElement("option");
+      option.value = categorie.toLowerCase();
+      option.textContent = categorie;
+
+      selectCategory.appendChild(option);
+    });
+
+    console.log("Nombre de projets:", projets.length);
+    console.log("Nombre de catégories:", categoriesArray.length);
+  });
+
+// ...
 
 function updateDisplayedProjects(projects) {
   let cartesHTML = document.getElementById("cartes");
-  cartesHTML.innerHTML = ""
+  cartesHTML.innerHTML = "";
 
   projects.forEach((projet) => {
-    createFigure(projet, cartesHTML)
+    createFigure(projet, cartesHTML);
   });
 }
 
@@ -76,6 +119,24 @@ const createFigure = (projet, cartesHTML) => {
   figure.appendChild(figcaption);
 
   cartesHTML.appendChild(figure);
+};
 
-  
+// Fonction pour mettre à jour la visibilité de la classe "ban"
+function updateBanVisibility() {
+  // Récupérer le token du stockage local
+  const token = localStorage.getItem("token");
+
+  // Sélectionner l'élément avec la classe "ban"
+  const banElement = document.querySelector(".ban");
+
+  // Vérifier si le token existe et si l'utilisateur est authentifié
+  if (token && token !== "null") {
+    // Afficher la classe "ban"
+    banElement.style.display = "flex";
+  } else {
+    // Masquer la classe "ban"
+    banElement.style.display = "none";
+  }
 }
+
+updateBanVisibility();
--- FrontEnd/js/modale.js
+++ FrontEnd/js/modale.js
@@ -1,36 +1,86 @@
-document.querySelector(".ouvrir").addEventListener("click", function() {
+document.querySelector(".ouvrir").addEventListener("click", function () {
   document.querySelector(".modal").style.display = "flex";
 });
 
-document.querySelector(".modal__close").addEventListener("click", function() {
-  document.querySelector(".modal").style.display = "none";
-});
-document.querySelector(".ouvrir").addEventListener("click", function() {
-  const modalBody = document.querySelector(".modal__body");
-  modalBody.innerHTML = ""; // Réinitialise le contenu de la modal__body
-
-  const cartes = document.getElementById("cartes").innerHTML;
-  modalBody.innerHTML = cartes;
-
-  document.querySelector(".modal").style.display = "flex";
-});
-
-document.querySelector(".modal__close").addEventListener("click", function() {
+document.querySelector(".modal__close").addEventListener("click", function () {
   document.querySelector(".modal").style.display = "none";
 });
 
-document.querySelector(".modal__footer_supprimer").addEventListener("click", function() {
-  const modalBody = document.querySelector(".modal__body");
-  modalBody.innerHTML = "";
-});
-document.querySelector(".modal__footer_button").addEventListener("click", function() {
-  // Afficher la deuxième modale
-  document.querySelector(".modal2").style.display = "flex";
+document
+  .querySelector(".modal__footer_supprimer")
+  .addEventListener("click", function () {
+    const modalBody = document.querySelector(".modal__body");
+    modalBody.innerHTML = "";
+  });
 
-  // Disparition de la première modale
-  document.querySelector(".modal").style.display = "none";
-});
-document.querySelector(".modal2__close").addEventListener("click", function() {
+document
+  .querySelector(".modal__footer_button")
+  .addEventListener("click", function () {
+    // Afficher la deuxième modale
+    document.querySelector(".modal2").style.display = "flex";
+
+    // Disparition de la première modale
+    document.querySelector(".modal").style.display = "none";
+  });
+
+document.querySelector(".modal2__close").addEventListener("click", function () {
   document.querySelector(".modal2").style.display = "none";
 });
-    
+
+// Fonction pour créer et ajouter une div d'image avec texte, lien d'édition et icône supprimer
+function createImageTextDiv(imageData) {
+  const modalBody = document.querySelector(".modal__body");
+
+  // Créer un conteneur pour l'image et le texte
+  const imageTextContainer = document.createElement("div");
+  imageTextContainer.classList.add("image-text-container");
+
+  // Créer un élément d'image
+  const imageElement = document.createElement("img");
+  imageElement.src = imageData.imageUrl;
+
+  // Créer un élément de texte
+  const textElement = document.createElement("p");
+  textElement.textContent = imageData.text;
+
+  // Créer un lien d'édition
+  const editLink = document.createElement("a");
+  editLink.href = "#"; // Ajoutez le lien approprié pour l'édition
+  editLink.textContent = "Éditer";
+
+  // Créer une icône supprimer
+  const deleteIcon = document.createElement("span");
+  deleteIcon.classList.add("delete-icon");
+  deleteIcon.innerHTML = "&#x2716;"; // Utilisez le code HTML ou l'icône de votre choix
+
+  // Ajouter l'image, le texte, le lien d'édition et l'icône supprimer au conteneur image-texte
+  imageTextContainer.appendChild(imageElement);
+  imageTextContainer.appendChild(textElement);
+  imageTextContainer.appendChild(editLink);
+  imageTextContainer.appendChild(deleteIcon);
+
+  // Ajouter le conteneur image-texte à la modal__body
+  modalBody.appendChild(imageTextContainer);
+
+  // Ajouter un gestionnaire d'événements à l'icône supprimer pour supprimer la div parent
+  deleteIcon.addEventListener("click", function () {
+    imageTextContainer.remove();
+  });
+}
+
+// Fonction pour charger les images depuis l'API avec fetch
+function fetchImages() {
+  fetch("http://localhost:5678/api/works", { method: "GET" })
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((imageData) => {
+        createImageTextDiv(imageData);
+      });
+    })
+    .catch((error) => {
+      console.error("Erreur lors du chargement des images :", error);
+    });
+}
+
+// Appel de la fonction pour charger les images depuis l'API
+fetchImages();
